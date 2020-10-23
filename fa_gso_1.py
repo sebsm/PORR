0d9779d06cb878695297e600fb46dbc450c6735b
selvert <01102827@pw.edu.pl>
2020-10-23 22:44:00 +0200

    Starting definitions

---
+++ fa_gso_1.py
@@ -0,0 +1,30 @@
+
+# dane 
+
+n_1=2   # liczba członków populacji
+n_2=10
+n_3=20
+n_4=50
+n_5=100
+
+alpha = 1.0 # parametr losowości, gdzie 1 - bardzo losowy
+beta_min = 1.0 # współczynnik atrakycjności
+gamma = 0.01 # wspólczynnik absorpcji światła/dążenia (?)
+theta = 0.95 # 
+d = 10 # liczba przestrzeni/wymiarów
+
+
+# Zadanie 1
+
+
+
+
+
+
+
+
+
+# Zadanie 2
+
+
+
